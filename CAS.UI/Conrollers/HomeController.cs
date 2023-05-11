cad8ef3dde4eb58fdccee7ef7974c207baebf5f7
MehrozQazi <mehrozqazi346@gmail.com>
2023-05-11 21:28:20 +0500

    basicconfig is added

---
+++ CAS.UI/Conrollers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CAS.UI.Conrollers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
