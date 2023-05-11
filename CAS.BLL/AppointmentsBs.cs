3739534ae8bbf15ec83e85449c907833eccc8ac9
MehrozQazi <mehrozqazi346@gmail.com>
2023-05-12 00:24:03 +0500

    Implement BLL Layer

---
+++ CAS.BLL/AppointmentsBs.cs
@@ -0,0 +1,52 @@
+﻿using CAS.BOL;
+using CAS.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAS.BLL
+{
+    public interface IAppointmentsBs
+    {
+        // 100% Abstractions
+        IEnumerable<Appointments> GetAll();
+        Appointments GetById(int id);
+        bool Insert(Appointments obj);
+        bool Update(Appointments obj);
+        bool Delete(int id);
+    }
+    public class AppointmentsBs : IAppointmentsBs
+    {
+        private readonly IAppointmentsDb objDb;
+        public AppointmentsBs(IAppointmentsDb _objDb)
+        {
+            objDb = _objDb;
+        }
+        public IEnumerable<Appointments> GetAll()
+        {
+            return objDb.GetAll();
+        }
+        public Appointments GetById(int id)
+        {
+            return objDb.GetById(id);
+        }
+        public bool Insert(Appointments obj)
+        {
+            return objDb.Insert(obj);
+        }
+        public bool Update(Appointments obj)
+        {
+            return objDb.Update(obj);
+        }
+        public bool Delete(int id)
+        {
+            return objDb.Delete(id);
+        }
+    }
+}
+
+
+
+
---
+++ CAS.BLL/DoctorsBs.cs
@@ -0,0 +1,50 @@
+﻿using CAS.BOL;
+using CAS.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAS.BLL
+{
+    public interface IDoctorsBs
+    {
+        // 100% Abstractions
+        IEnumerable<Doctors> GetAll();
+        Doctors GetById(int id);
+        bool Insert(Doctors obj);
+        bool Update(Doctors obj);
+        bool Delete(int id);
+    }
+    public class DoctorsBs : IDoctorsBs
+    {
+        private readonly IDoctorsDb objDb;
+        public DoctorsBs(IDoctorsDb _objDb)
+        {
+            objDb = _objDb;
+        }
+        public IEnumerable<Doctors> GetAll()
+        {
+            return objDb.GetAll();
+        }
+        public Doctors GetById(int id)
+        {
+            return objDb.GetById(id);
+        }
+        public bool Insert(Doctors obj)
+        {
+            return objDb.Insert(obj);
+        }
+        public bool Update(Doctors obj)
+        {
+            return objDb.Update(obj);
+        }
+        public bool Delete(int id)
+        {
+            return objDb.Delete(id);
+        }
+    }
+}
+
+
