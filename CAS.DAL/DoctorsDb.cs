b7102dd5b20ba6b2a16bf029bc39841cc1cfaf76
MehrozQazi <132157932+MehrozQazi@users.noreply.github.com>
2023-05-11 23:43:22 +0500

    Story/1002/mehroz.qazi/ImplementDALLayer (#3)
    * added comment for testing.
    
    * models is added
    
    * Implement DAL Layer

---
+++ CAS.DAL/DoctorsDb.cs
@@ -0,0 +1,59 @@
+﻿using CAS.BOL;
+using CAS.DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAS.DAL
+{
+    public interface IDoctorsDb
+    {
+        // 100% Abstractions
+        IEnumerable<Doctors> GetAll();
+        Doctors GetById(int id);
+        bool Insert(Doctors obj);
+        bool Update(Doctors obj);
+        bool Delete(int id);
+    }
+    public class DoctorsDb : IDoctorsDb
+    {
+        // Standard pattern --> Repository
+        private AppDbContext context;
+        public DoctorsDb(AppDbContext _context)
+        {
+            context = _context;
+        }
+        public IEnumerable<Doctors> GetAll()
+        {
+            // Add,Update,Remove
+            return context.Doctors;
+        }
+        public Doctors GetById(int id)
+        {
+            var obj = context.Doctors.Find(id);
+            return obj;
+        }
+        public bool Insert(Doctors obj)
+        {
+            context.Doctors.Add(obj);
+            context.SaveChanges();
+            return true;
+        }
+        public bool Update(Doctors obj)
+        {
+            context.Doctors.Update(obj);
+            context.SaveChanges();
+            return true;
+        }
+        public bool Delete(int id)
+        {
+            var obj = context.Doctors.Find(id);
+            context.Doctors.Remove(obj);
+            context.SaveChanges();
+            return true;
+        }
+    }
+}
+
---
+++ CAS.DAL/PatientsDb.cs
@@ -0,0 +1,59 @@
+﻿using CAS.BOL;
+using CAS.DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAS.DAL
+{
+    public interface IPatientsDb
+    {
+        // 100% Abstractions
+        IEnumerable<Patients> GetAll();
+        Patients GetById(int id);
+        bool Insert(Patients obj);
+        bool Update(Patients obj);
+        bool Delete(int id);
+    }
+    public class PatientsDb : IPatientsDb
+    {
+        // Standard pattern --> Repository
+        private AppDbContext context;
+        public PatientsDb(AppDbContext _context)
+        {
+            context = _context;
+        }
+        public IEnumerable<Patients> GetAll()
+        {
+            // Add,Update,Remove
+            return context.Patients;
+        }
+        public Patients GetById(int id)
+        {
+            var obj = context.Patients.Find(id);
+            return obj;
+        }
+        public bool Insert(Patients obj)
+        {
+            context.Patients.Add(obj);
+            context.SaveChanges();
+            return true;
+        }
+        public bool Update(Patients obj)
+        {
+            context.Patients.Update(obj);
+            context.SaveChanges();
+            return true;
+        }
+        public bool Delete(int id)
+        {
+            var obj = context.Patients.Find(id);
+            context.Patients.Remove(obj);
+            context.SaveChanges();
+            return true;
+        }
+    }
+}
+
