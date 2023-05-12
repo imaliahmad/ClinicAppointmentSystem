06cfdb6e96ccb295aee6bf8e00d0e6b1533639c1
MehrozQazi <132157932+MehrozQazi@users.noreply.github.com>
2023-05-12 20:24:37 +0500

    Implement BLL Layer (#5)
    * added comment for testing.
    
    * models is added
    
    * Implement DAL Layer
    
    * Implement BLL Layer
    
    * Implement BLL Layer

---
+++ CAS.BLL/PatientsBs.cs
@@ -0,0 +1,52 @@
+﻿using CAS.BOL;
+using CAS.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAS.BLL
+{
+    public interface IPatientsBs
+    {
+        // 100% Abstractions
+        IEnumerable<Patients> GetAll();
+        Patients GetById(int id);
+        bool Insert(Patients obj);
+        bool Update(Patients obj);
+        bool Delete(int id);
+    }
+    public class PatientsBs : IPatientsBs
+    {
+        private readonly IPatientsDb objDb;
+        public PatientsBs(IPatientsDb _objDb)
+        {
+            objDb = _objDb;
+        }
+        public IEnumerable<Patients> GetAll()
+        {
+            return objDb.GetAll();
+        }
+        public Patients GetById(int id)
+        {
+            return objDb.GetById(id);
+        }
+        public bool Insert(Patients obj)
+        {
+            return objDb.Insert(obj);
+        }
+        public bool Update(Patients obj)
+        {
+            return objDb.Update(obj);
+        }
+        public bool Delete(int id)
+        {
+            return objDb.Delete(id);
+        }
+    }
+}
+
+
+
+
